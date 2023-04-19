c4e33234560976f4f0fa1db5c82368dc2f611a57
Inzamamazeez <inzamamazeez014@gmail.com>
2023-04-20 01:00:03 +0530

    Inzamamreact 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Reactlogo() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+   <img src={logo} className="App-logo" alt="logo" />
+   </>
+  )
 }
-
+function Reactbenifits(){
+  return(
+    <>
+    <h1>Benifits of using React...</h1>
+    <ol>
+      <l1>Component-based architecture</l1>
+      <l2>Virtual DOM for efficient updates</l2>
+      <l3>Rich ecosystem and community</l3>
+      <l4>Cross platform development</l4>
+      <l5>Strong community support:</l5>
+    </ol>
+    </>
+  )
+}
+  function Button(){
+    return(
+      <Button>Get started...</Button>
+    )
+  }
+  function App(){
+    return(
+      <div className="App">
+      <Reactlogo/>
+      <Reactbenifits/>
+      <Button/>
+      </div>
+    )
+  }
 export default App;
